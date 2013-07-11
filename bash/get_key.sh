c28c3cc1cd142f511f48e5b7c8e8cd31a83aa2fa
tombye <tombaromba@gmail.com>
2013-07-11 16:32:16 +0100

    Add get_key

---
+++ bash/get_key.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+function usage {
+  cat << EOF
+  Usage: $0 <path-to-key>
+  
+  Show fingerprint of specified public key file
+
+EOF
+}
+
+if [ $# -ne 1 ]; then
+  usage
+  exit 1
+fi
+
+ssh-keygen -l -f $1
