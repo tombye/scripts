b95780f36219810fc39de9b9cc96aa8b4ad37101
Tom Byers <tombaromba@gmail.com>
2014-02-07 09:22:31 +0000

    Add script to remove comments in SCSS/CSS files

---
+++ bash/remove_scss_comments.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Mostly borrowed from: https://github.com/duckinator/duckinator.net/blob/master/_minify-css.sh
+
+function usage {
+  cat << EOF
+  Usage: $0 [options]
+
+  Strip CSS comments from all files in the directory
+
+  OPTIONS:
+    -f format of file (lowercase, no period) to search, defaults to scss
+
+EOF
+}
+
+FORMAT='scss'
+
+while getopts "f" OPTION; do
+  case $OPTION in
+    f )
+      FORMAT=$OPTION
+      ;;
+  esac
+done
+
+shift $(($OPTIND-1))
+
+for file in `find . -name *.$FORMAT`
+do
+  # remove single-line starred comments 
+  sed -i '' 's|/\*.*\*/||g' $file
+  # remove multi-line comments
+  sed -i '' '/\/\*/,/\*\//d' $file
+  # remove single-line non-starred comments 
+  sed -i '' 's|//.*||g' $file
+  # remove left-over blank lines
+  sed -i '' '/^$/d' $file
+done;
